bd3945f3a0b198ffbf3ca87f4fdc3254ce415545
chloe-tt <chloe.trottin.chine@gmail.com>
2020-04-29 20:05:25 +0200

    header desktop + mobile done

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+
+function Header() {
+  return (
+    <div className="headerContainer">
+        <img className="logo" src="https://zupimages.net/up/20/18/eqv3.png" alt="" />
+        </div>
+  );
+}
+
+export default Header;
