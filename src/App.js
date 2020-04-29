f0f12b0a4fc27af20e5590f5b396b50d5fcbfd47
chloe-tt <chloe.trottin.chine@gmail.com>
2020-04-29 20:04:21 +0200

    update app.js et css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
+    <body>
     <div className="App">
+      <Header />
      <h1> InstaPaint Mother Flower</h1>
     </div>
+    </body>
   );
 }
 
